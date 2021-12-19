f12cf84e90b0f02aee9aa52346b6327f716baf61
Kody Fintak <kody.fintak@gmail.com>
2021-12-19 00:46:31 -0800

    delete sample code

--- day19/src/day19/core.clj
+++ day19/src/day19/core.clj
@@ -1,9 +1,5 @@
 (ns day19.core)
 
-(defn add
-  [a b]
-  (+ a b))
-
 (defrecord Coordinates [x y z])
 
 (defn parse-beacon [input]
--- day19/test/day19/core_test.clj
+++ day19/test/day19/core_test.clj
@@ -3,10 +3,6 @@
             [day19.core :refer :all])
   (:import (day19.core Coordinates)))
 
-(deftest adding
-  (testing "Adding two numbers"
-    (is (= (add 1 1) 2))))
-
 (deftest scanner-report
   (testing "Parse scanner report"
     (is (= (parse-beacon "0,0,0") (Coordinates. 0 0 0)))))
