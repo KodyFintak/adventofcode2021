ab1f8360177e915934d30337e987b282ded629a6
Kody Fintak <kody.fintak@gmail.com>
2021-12-19 00:45:29 -0800

    Parse 0,0,0 from report

--- day19/src/day19/core.clj
+++ day19/src/day19/core.clj
@@ -3,3 +3,8 @@
 (defn add
   [a b]
   (+ a b))
+
+(defrecord Coordinates [x y z])
+
+(defn parse-beacon [input]
+  (Coordinates. 0 0 0))
--- day19/test/day19/core_test.clj
+++ day19/test/day19/core_test.clj
@@ -1,7 +1,12 @@
 (ns day19.core-test
   (:require [clojure.test :refer :all]
-            [day19.core :refer :all]))
+            [day19.core :refer :all])
+  (:import (day19.core Coordinates)))
 
 (deftest adding
   (testing "Adding two numbers"
     (is (= (add 1 1) 2))))
+
+(deftest scanner-report
+  (testing "Parse scanner report"
+    (is (= (parse-beacon "0,0,0") (Coordinates. 0 0 0)))))
