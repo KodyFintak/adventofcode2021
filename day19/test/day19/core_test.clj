a96972e08e81861c8819554bed007d713fbd10be
Kody Fintak <kody.fintak@gmail.com>
2021-12-20 10:59:55 -0800

    Fix formatting of tests

--- day19/test/day19/core_test.clj
+++ day19/test/day19/core_test.clj
@@ -6,9 +6,11 @@
 (deftest scanner-report
   (testing "Parse scanner report"
     (is (= (parse-report "--- scanner 0 ---\n100,100,100") (list (Coordinates. 100 100 100))))
+    )
 
   (testing "Parse scanner report row"
     (is (= (parse-beacon "0,0,0") (Coordinates. 0 0 0)))
     (is (= (parse-beacon "0,0,1") (Coordinates. 0 0 1)))
     (is (= (parse-beacon "-100,100,20") (Coordinates. -100 100 20)))
-    )))
+    )
+  )
